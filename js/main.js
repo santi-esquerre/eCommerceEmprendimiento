92a08064aadca7519352f38ff1e37f14feaeaec8
Santiago Esquerré Sotelo <bymaxxeryt@gmail.com>
2023-10-30 11:34:32 -0300

    feat: done index

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function loadNavBar() {
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+    });
+}
+document.addEventListener("DOMContentLoaded", loadNavBar);
